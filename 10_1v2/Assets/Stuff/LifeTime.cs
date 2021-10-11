9a861001423955ee319145803fe9a190c102c642
Lujie Zhao <lujie.sue.zhao@gmail.com>
2021-10-10 20:20:12 -0400

    Clean up the IL2CPP cache and initialize the project

---
+++ 10_1v2/Assets/Stuff/LifeTime.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int life = 2;
+    void Start()
+    {
+        Destroy(gameObject, life);
+    }
+
+
+}
